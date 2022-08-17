8feb83a88a58edf3171f4d6fe1e00431bcca0d2c
PaulusGriseo <pawel@greszta.pl>
2022-08-17 16:30:53 +0200

    Add zmiany kontrolne

--- modul_7_1.py
+++ modul_7_1.py
@@ -3,19 +3,23 @@ from faker import Faker
 
 fake = Faker('pl_PL')
 
-class Person:
-    def __init__(self, surname, name, company, job, email_address) -> None:
+#logging.basicConfig(level=logging.INFO)
+
+private_contcts = []
+bussines_contcts = []
+
+class BaseContact:
+    def __init__(self, name, surname, phone, email_address) -> None:
         self.name = name
         self.surname = surname
-        self.company = company
-        self.occupation = job
+        self.phone = phone
         self.email_address = email_address
 
         self._label_lenth = 0
 
     def contact(self):
         info = ("Kontaktuję się z ->")
-        result = print(f"{info} {self.name} {self.surname}; {self.company}; {self.email_address}")
+        result = print(f"{info} {self.name} {self.surname}; {self.phone}; {self.email_address}")
 
     @property
     def label_lenth(self):
@@ -24,7 +28,26 @@ class Person:
         print(f"Suma znaków imienia i nzwiska to: {value}")
         return self._label_lenth
 
-for Card in range(1):
-    Card = Person(name:=fake.last_name(), surname:=fake.first_name(), company:=fake.company(), job:=fake.job(), email_address:=fake.email())
-    Card.contact()
-    Card.label_lenth
+class BussinesContact(BaseContact):
+    def __init__(self, bussines_phone, company, job, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.company = company
+        self.job = job
+        self.bussines_phone = bussines_phone
+
+
+def create_contacts(aspect, quantity):
+    if aspect == BaseContact:
+        for contact in range(quantity):
+            contact = aspect(name:=fake.first_name(), surname:=fake.last_name(),phone:=fake.phone_number(), email_address:=fake.email())
+    elif aspect == BussinesContact:
+        for contact in range(quantity):
+            contact = aspect(name:=fake.last_name(), surname:=fake.first_name(),bussines_phone:=fake.phone_number(), email_address:=fake.email(), company:=fake.company(), job:=fake.job())
+            
+    
+create_contacts(BaseContact, 10)
+#create_contacts(BussinesContact, 10)
+
+
+#Card.contact()
+#Card.label_lenth
